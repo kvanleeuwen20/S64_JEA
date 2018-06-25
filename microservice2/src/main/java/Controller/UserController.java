4a8ce1e76a1950063eb568bded7264c3226eb25b
jehama <jehama@users.noreply.github.com>
2018-06-25 18:56:45 +0200

    added communication between the 2 microservices for authentication.

--- microservice2/src/main/java/Controller/UserController.java
+++ microservice2/src/main/java/Controller/UserController.java
@@ -3,10 +3,8 @@ package Controller;
 import DTO.UserDTO;
 import Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.stream.Collectors;
